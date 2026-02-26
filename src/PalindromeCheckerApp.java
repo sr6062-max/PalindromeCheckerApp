9d547d411f4177e0405e90cf97352a10c81595df
sr6062-max <sr6062.srmist.edu.in>
2026-02-26 15:03:28 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,10 +1,21 @@
 import java.sql.SQLOutput;
 
 public class PalindromeCheckerApp {
-public static void main(String[] args) {
-    System.out.println("Welcome to the palindrome checker Management System.");
-    System.out.println("Version : 1.0");
-    System.out.println("System initialized successfully");
 
-}
+    public static void main(String[] args) {
+
+        String word = "madam";
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < word.length() / 2; i++) {
+
+            if (word.charAt(i) != word.charAt(word.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input text: " + word);
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
+    }
 }
