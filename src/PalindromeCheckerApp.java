7198500e7d297a001eb58f60b4adfe14ff9f7141
sr6062-max <sr6062.srmist.edu.in>
2026-02-26 14:42:30 +0530

    UC1: Add Welcome Message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,10 @@
+import java.sql.SQLOutput;
+
 public class PalindromeCheckerApp {
+public static void main(String[] args) {
+    System.out.println("Welcome to the palindrome checker Management System.");
+    System.out.println("Version : 1.0");
+    System.out.println("System initialized successfully");
+
+}
 }
