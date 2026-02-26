d76472a97776f92622db68b5dd0a2a0b31412e96
sr6062-max <sr6062.srmist.edu.in>
2026-02-26 15:50:12 +0530

    UC4:checkout palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -3,15 +3,20 @@ import java.sql.SQLOutput;
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
-         String input = "madam";
-                String reversed = "";
-                for (int i = input.length() - 1; i >= 0; i--) {
-                    reversed = reversed + input.charAt(i);
+                String input = "radar";
+                char[] chars = input.toCharArray();
+                int start = 0;
+                int end = chars.length - 1;
+                boolean isPalindrome = true;
+                while (start < end) {
+                    if (chars[start] != chars[end]) {
+                        isPalindrome = false;
+                        break;
+                    }
+                    start++;
+                    end--;
                 }
-                boolean isPalindrome = input.equals(reversed);
-                System.out.println("Original String  : " + input);
-                System.out.println("Reversed String  : " + reversed);
-                System.out.println("Is it a Palindrome? : " + isPalindrome);
-                System.out.println("------------") ;
-    }
+                System.out.println("Input text : " + input);
+                System.out.println("Is Palindrome? : " + isPalindrome);
+            }
         }
